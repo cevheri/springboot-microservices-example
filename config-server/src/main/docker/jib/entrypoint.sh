170e89274336cf7b5863ff88941bdf9f60f6f632
Cevheri <cevheribozoglan@gmail.com>
2021-06-12 00:45:45 +0300

    configure docker files. added google jib library.

---
+++ config-server/src/main/docker/jib/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "The application will start in ..."
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.cevher.ms.config.ConfigServerApplication"  "$@"
