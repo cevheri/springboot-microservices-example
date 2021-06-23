204ba08055bc5dc622d92e309eff3e488a98c0f5
Cevheri <cevheribozoglan@gmail.com>
2021-06-23 20:09:06 +0300

    Added Spring Cloud refresh scope. Refreshing your properties using Spring Cloud configuration server

--- person-service/src/main/java/com/cevher/ms/person/PersonServiceApplication.java
+++ person-service/src/main/java/com/cevher/ms/person/PersonServiceApplication.java
@@ -3,12 +3,14 @@ package com.cevher.ms.person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class PersonServiceApplication {
 
 	public static void main(String[] args) {
