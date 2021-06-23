10dd0aca0751e7c696538ddb005ce8d29997fcc2
Cevheri <cevheribozoglan@gmail.com>
2021-06-23 22:20:40 +0300

    Looking up service instances with Spring DiscoveryClient

---
+++ person-service/src/main/java/com/cevher/ms/person/config/DepartmentDiscoveryClient.java
@@ -0,0 +1,48 @@
+package com.cevher.ms.person.config;
+
+import com.cevher.ms.person.vm.DepartmentVM;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * Looking up service instances with Spring DiscoveryClient. The Spring DiscoveryClient offers
+ * the lowest level of access to Ribbon and the services registered within it. Using the DiscoveryClient,
+ * you can query for all the services registered with the ribbon client and their corresponding URLs.
+ * Next, you’ll build a simple example of using the DiscoveryClient to retrieve one of
+ * the organization service URLs from Ribbon and then call the service using a standard RestTemplate class.
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DepartmentDiscoveryClient {
+
+    private final DiscoveryClient discoveryClient;
+
+    public DepartmentVM getDepartment(String departmentId) {
+        RestTemplate restTemplate = new RestTemplate();
+        List<ServiceInstance> instances = discoveryClient.getInstances("department-service");
+
+        if (instances.size() == 0) return null;
+        String serviceUri = String.format("%s/v1/departments/%s", instances.get(0).getUri().toString(), departmentId);
+        log.info("!!!! SERVICE URI:  " + serviceUri);
+
+        val restExchange = restTemplate.exchange(
+                serviceUri,
+                HttpMethod.GET,
+                null,
+                DepartmentVM.class,
+                departmentId);
+
+        return restExchange.getBody();
+    }
+}
