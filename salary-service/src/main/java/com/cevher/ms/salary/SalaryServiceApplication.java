35ca320bb36d7fe1bb86d01ce12605a7840a6270
Cevheri <cevheribozoglan@gmail.com>
2021-07-08 13:19:42 +0300

    Salary Microservices added. with docker build and config server

--- salary-service/src/main/java/com/cevher/ms/salary/SalaryServiceApplication.java
+++ salary-service/src/main/java/com/cevher/ms/salary/SalaryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.cevher.ms.salaryservice;
+package com.cevher.ms.salary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- salary-service/src/test/java/com/cevher/ms/salary/SalaryServiceApplicationTests.java
+++ salary-service/src/test/java/com/cevher/ms/salary/SalaryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cevher.ms.salaryservice;
+package com.cevher.ms.salary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
